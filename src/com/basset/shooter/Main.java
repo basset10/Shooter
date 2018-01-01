3780d23c8ca3ef0db0f1813668907bf9b6a002d6
Zack <Zack@DESKTOP-T4SQJO2>
2017-12-31 23:34:22 -0600

    made changes

--- src/com/basset/shooter/Main.java
+++ src/com/basset/shooter/Main.java
@@ -1,5 +1,28 @@
 package com.basset.shooter;
 
-public class Main {
-//sdfsd
+import com.osreboot.ridhvl.display.HvlDisplayMode;
+import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
+import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
+
+
+public class Main extends HvlTemplateInteg2D{
+	public static void main(String[] args) {
+		new Main();
+	}
+
+	public Main() {
+		super(200, 1280, 720, "r3kt", new HvlDisplayModeDefault());
+	}
+
+	@Override
+	public void initialize() {
+	}
+
+	@Override
+	public void update(float delta) {
+	}
+
+
+
+
 }
