d44dd8158dafed161b94fa4b4b30ae0a43d170ee
Zack <Zack@DESKTOP-T4SQJO2>
2018-01-03 03:15:59 -0600

    MADE SQUARE MAN MOVE BETER

--- src/com/basset/shooter/Game.java
+++ src/com/basset/shooter/Game.java
@@ -12,12 +12,16 @@ public class Game {
 	static final float MAX_SPEED = 300;
 	static final float ACCELERATION = 3500.0f;
 	static final int PLAYER_SIZE = 10;
+	static final int BOX_X = 500;
+	static final int BOX_Y = 500;
+	static final int BOX_SIZE = 50;
+
 	static float xPosition = 1280/2;
 	static float yPosition = 720/2;
 	static float playerSpeed = 1.0f;
 	static float xSpeed = 0.0f;
 	static float ySpeed = 0.0f;
-	
+
 
 	static void initialize(){
 
@@ -26,43 +30,52 @@ public class Game {
 	static void update(float delta) {
 		movePlayer(delta);
 		hvlDrawQuadc(xPosition, yPosition, PLAYER_SIZE, PLAYER_SIZE, Color.gray);
-		hvlDrawQuadc(500, 250, 50, 50, Color.red);
+		hvlDrawQuadc(BOX_X, BOX_X, BOX_SIZE, BOX_SIZE, Color.red);
 	}
 
 	static void movePlayer(float delta) {
 		//Vertical Movement
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
 			ySpeed = ySpeed - (delta * ACCELERATION);
-			if(ySpeed <= -MAX_SPEED) {
-				ySpeed = -MAX_SPEED;
-			}
 		}else if(Keyboard.isKeyDown(Keyboard.KEY_S)){
 			ySpeed = ySpeed + (delta * ACCELERATION);
-			if(ySpeed >= MAX_SPEED) {
-				ySpeed = MAX_SPEED;
-			}
 		}else{
 			ySpeed = HvlMath.stepTowards(ySpeed, ACCELERATION*delta, 0.0f);
 		}
-		
+
 		//Horizontal Movement
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
 			xSpeed = xSpeed - (delta * ACCELERATION);
-			if(xSpeed <= -MAX_SPEED) {
-				xSpeed = -MAX_SPEED;
-			}
 		}else if(Keyboard.isKeyDown(Keyboard.KEY_D)){
 			xSpeed = xSpeed + (delta * ACCELERATION);
-			if(xSpeed >= MAX_SPEED) {
-				xSpeed = MAX_SPEED;
-			}
 		}else{
 			xSpeed = HvlMath.stepTowards(xSpeed, ACCELERATION*delta, 0.0f);
 		}
+		
+		if(yPosition >= BOX_Y - (BOX_SIZE/2) - (PLAYER_SIZE/2) && yPosition <= BOX_Y + (BOX_SIZE/2) + (PLAYER_SIZE/2) &&
+				xPosition >= BOX_X - (BOX_SIZE/2) - (PLAYER_SIZE/2) && xPosition <= BOX_X + (BOX_SIZE/2) + (PLAYER_SIZE/2)) {
+			ySpeed = (yPosition - BOX_Y) * 8;
+			xSpeed = (xPosition - BOX_X) * 8;
+			
+		}
+		
+		if(ySpeed <= -MAX_SPEED) {
+			ySpeed = -MAX_SPEED;
+		}
+		if(ySpeed >= MAX_SPEED) {
+			ySpeed = MAX_SPEED;
+		}
+		if(xSpeed <= -MAX_SPEED) {
+			xSpeed = -MAX_SPEED;
+		}
+		if(xSpeed >= MAX_SPEED) {
+			xSpeed = MAX_SPEED;
+		}
+		
 		yPosition = HvlMath.limit(yPosition + (delta * ySpeed), PLAYER_SIZE/2, 720 - (PLAYER_SIZE/2));
 		xPosition = HvlMath.limit(xPosition + (delta * xSpeed), PLAYER_SIZE/2, 1280 - (PLAYER_SIZE/2));
 	}
-	
+
 	static void restart(){
 		xPosition = 1280/2;
 		yPosition = 720/2;
@@ -70,5 +83,5 @@ public class Game {
 		xSpeed = 0.0f;
 		ySpeed = 0.0f;
 	}
-	
+
 }
